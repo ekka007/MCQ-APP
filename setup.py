962cb540b64cb1107c54cd32614f234784f4c015
saroj <you@example.com>
2023-12-11 17:07:31 +0530

    file-updated

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
 # install local packages for that reason it requires setup.py
 
+from setuptools import find_packages,setup
+
+setup(
+    name='py_utils',
+    version='0.0.1',
+    author='saroj',
+    author_email='sarojekka@cloudgenx.com',
+    install_requires=['openai','langchain','streamlit','python-dotenv','pyPDF2'],
+    packages=find_packages(),
+)
